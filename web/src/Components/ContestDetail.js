cfb57e3eb5fc09f06e25a933ad95aa2d079675ad
Maria Arevalo <mi.arevalo10@uniandes.edu.co>
2018-08-30 02:15:36 -0500

    fixed error with axios-api route in submissions

--- web/src/Components/ContestDetail.js
+++ web/src/Components/ContestDetail.js
@@ -10,6 +10,9 @@ import Typography from '@material-ui/core/Typography';
 import GridList from '@material-ui/core/GridList';
 import instance from "../AjaxCrtl";
 import Header from "./Header";
+import Grow from "@material-ui/core/es/Grow/Grow";
+import TextField from "@material-ui/core/es/TextField/TextField";
+import Grid from "@material-ui/core/es/Grid/Grid";
 
 
 class ContestDetail extends Component {
@@ -24,67 +27,83 @@ class ContestDetail extends Component {
     }
 
     componentDidMount(){
-        console.log("cualquier cosa")
-        instance().get('contest/'+this.state.id+'/submissions')
+        var url = '/contest/1/submissions';
+
+        instance().get('contest/'+this.props.match.params.contestId+'/submissions')
             .then((response) => {
                 this.setState({submissions:response.data})
                 console.log(response);
             })
             .catch((error) => {
-                console.log('entra',error);
+                console.log(error.response)
             });
     }
 
     render() {
         const { classes } = this.props;
 
-        const cards = this.state.submissions.map((tile) => <Card key={tile.id} >
-            <CardMedia
-                className={classes.media}
-                image="/images.jpeg"
-                title="Contemplative Reptile"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="headline" component="h2">
-                    {tile.firstName} {tile.lastName}
-                </Typography>
-                <Typography component="p">
-                    <strong>Email: </strong>{tile.email}
-                </Typography>
-                <Typography component="p">
-                    <strong>Fecha:</strong> {tile.date}
-                </Typography>
-                <Typography component="p">
-                    <strong>Estado:</strong> {tile.state}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small" color="primary">
-                    Ver original
-                </Button>
-                <Button size="small" color="primary">
-                    Ver video convertido
-                </Button>
-            </CardActions>
-        </Card>);
-
         const props = this.props;
 
-
         return (
-            <div className="main" style={{ marginTop: '75px' }}>
+            <div className="main" style={{ marginTop: '75px' }} >
                 <MuiThemeProvider theme={THEME}>
                     <Header
                         {...props}
                         brand={'Content manager'}
                         color={'info'}/>
-                    {(this.state.submissions !== undefined && this.state.submissions !== {}) &&
-                    <GridList className={classes.root} cellHeight={'auto'} cols={5} >
-                        {React.Children.toArray(cards)}
-                    </GridList>}
+                    { this.state.submissions ? (
+                        <div>
+                            {/*<TextField style={{padding: 24}}
+                                           id="searchInput"
+                                           placeholder="Search for Submissions"
+                                           margin="normal"
+                                />*/}
+                            <Grid container spacing={24} style={{padding: 24}}>
+                                { this.state.submissions.map(tile => (
+                                    <Grid item xs={12} sm={4} md={3} xl={3}>
+                                        <Card style={{padding: 10}} key={tile.id} >
+                                            <CardMedia
+                                                className={classes.media}
+                                                image="/images.jpeg"
+                                                title="video"
+                                            />
+                                            <CardContent>
+                                                <Typography gutterBottom variant="headline" component="h2">
+                                                    {tile.firstName} {tile.lastName}
+                                                </Typography>
+                                                <Typography component="p">
+                                                    <strong>Email: </strong>{tile.email}
+                                                </Typography>
+                                                <Typography component="p">
+                                                    <strong>Fecha:</strong> {tile.date}
+                                                </Typography>
+                                                <Typography component="p">
+                                                    <strong>Estado:</strong> {tile.state}
+                                                </Typography>
+                                            </CardContent>
+                                            <CardActions>
+                                                <Button size="small" color="primary">
+                                                    Ver original
+                                                </Button>
+                                                <Button size="small" color="primary">
+                                                    Ver video convertido
+                                                </Button>
+                                            </CardActions>
+                                        </Card>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </div>
+                    ) : "No courses found" }
                 </MuiThemeProvider>
 
+
+
             </div>
+
+
+
+
         );
     }
 }
@@ -93,19 +112,17 @@ class ContestDetail extends Component {
 const styles = {
 
     media: {
-        height: 140,
+        height: 0,
+        paddingTop: '56.25%'
     },
     root: {
         display: 'flex',
         flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden'
-    },
-    gridList: {
-        width: 100,
-        height: 450,
-    },
-
+        //justifyContent: 'space-around',
+        overflow: 'hidden',
+        padding: 50,
+        height: 'auto',
+    }
 };
 
 const THEME = createMuiTheme({
