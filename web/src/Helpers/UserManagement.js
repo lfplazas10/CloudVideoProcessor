b99ad6040bffa1d807aaa1f83cb504fac4400db5
Luis Plazas <lf.plazas10@uniandes.edu.co>
2018-09-05 16:36:18 -0500

    Added automatic cookie removal when logging out or when cookie expires and it's no longer recognized by the server

--- web/src/Helpers/UserManagement.js
+++ web/src/Helpers/UserManagement.js
@@ -1,11 +1,14 @@
 import instance from './AjaxCrtl.js'
 import browserHistory from '../BrowserHistory.js';
+import cookie from 'react-cookies'
 
 export default function (){
   instance().get('active')
     .then(response => {
-      if (!response.data)
+      if (!response.data) {
         browserHistory.push('/');
+        cookie.remove('PLAY_SESSION');
+      }
     }).catch(error => {
       console.log(error);
   });
