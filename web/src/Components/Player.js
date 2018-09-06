bf62fd940810c32f5aa612c356b693bebc1aafee
Luis Plazas <lf.plazas10@uniandes.edu.co>
2018-09-05 23:37:24 -0500

    Added the option to download a video if it's encoded with an unsupported format. This will only work in production (already tested)

--- web/src/Components/Player.js
+++ web/src/Components/Player.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
-import Button from "@material-ui/core/es/Button/Button";
+import Button from "@material-ui/core/Button/Button";
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 
 class Player extends Component {
   
@@ -11,7 +13,8 @@ class Player extends Component {
       success: false,
       loading: false,
       open: true,
-      error: null
+      error: null,
+      cantPlay: false
     };
   }
   
@@ -19,10 +22,28 @@ class Player extends Component {
     return (
       <div>
         <Dialog open={this.state.open}>
-          <video width="100%" height="100%" controls>
+          <video width="100%" height="100%"
+                 onError={()=>this.setState({cantPlay: true})}
+                 controls>
             <source src={this.props.videoSrc} type={this.props.videoType}/>
           </video>
+          {this.state.cantPlay &&
+          <DialogContent className='playerDialog'>
+            <DialogContentText id="alert-dialog-slide-description">
+              Unfortunately the player is not reproduce this  video format.
+              In a couple of minutes it'll be ready to watch on a supported format.
+              Meanwhile, you can download it and watch it on your PC using VLC
+            </DialogContentText>
+          </DialogContent>
+          }
           <DialogActions>
+            {/*This works, but only on production because of the react proxy server*/}
+            {this.state.cantPlay &&
+            <Button color="primary" onClick={() => document.getElementById('link').click()}>
+              Download
+            </Button>
+            }
+            <a id="link" href={this.props.videoSrc} download hidden></a>
             <Button onClick={this.props.togglePlayer} color="primary">
               Cancel
             </Button>
