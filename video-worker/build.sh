f74caa6bf66b967e2fdb06e54290e8f3a68facd8
Luis Plazas <lf.plazas10@uniandes.edu.co>
2018-09-16 14:12:18 -0500

    Added web bug fixed to production folder. Modified docker images, will now use them from a different user account

--- video-worker/build.sh
+++ video-worker/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Remember to export all necessary env vars before running this script
 if [ "$1" == "true" ]; then
-    docker build . -t lfplazas10/video-worker
+    docker build . -t development2018/video-worker
 fi
 if [ "$2" == "true" ]; then
-    docker push lfplazas10/video-worker
+    docker push development2018/video-worker
 fi
