8a8919cd62d6909b8e79030c0844bdfb27aa058d
Luis Plazas <lf.plazas10@uniandes.edu.co>
2018-09-22 19:33:03 -0500

    Modified entrypoint for server app

--- Dockerfiles/entry-point.sh
+++ Dockerfiles/entry-point.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 mount -t nfs4 -o proto=tcp,port=2049 ${NFS_IP}:/ /root/server/nfs/
+if [ "$?" != 0 ]; then
+    echo "ERROR: NFS share failed to mount: $?"
+    exit -1;
+else
+    echo "NFS share mount successfully"
+fi
 /root/server/bin/./contest-server -Dplay.evolutions.db.default.autoApply=true
